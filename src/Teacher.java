a9cb2c36439f0fcbc0ca1eb2ad03f4d1ce51d355
Author <PC@Dell>
2017-11-21 15:34:36 -0500

    abcd

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,10 +1,36 @@
 import java.awt.Color;
 
 public class Teacher extends Person{
-
-	public Teacher(String firstName, String familyName, Color eyeColor, int IQ) 
+	
+	private String subject;
+	private String title;
+	
+	public Teacher(String firstName, String familyName, Color eyeColor, int IQ, String subject, String title) 
 	{
 		super(firstName, familyName, eyeColor, IQ);
+		this.subject = subject;
+		this.title = title;
+	}
+
+	private String getSubject() {
+		return subject;
+	}
+
+	private void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	private String getTitle() {
+		return title;
+	}
+
+	private void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
