050fa32fd39bb7f55d31df0724d0cd2800004719
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-11-28 19:21:16 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,20 @@
-
 public class Runner {
 
+	private static String[] firstName = {"First", "John", "Jane", "Nathan", "Joe", "Steve", "Liam", "Noah", "Jake", "Logan", "David"};
 	public static void main(String[] args) {
 	
-		Person Teacher = new Teacher();
-		Person Student = new Student();
-		Classroom Classroom = new Classroom();
+		new Teacher(null, null, null, 0, null, null);
+		Person Student = new Student(null, null, null, 0, 0, 0, 0);
+		Classroom Classroom = new Classroom(null, null, null, 0, null, Student);
 		
-		String[] firstName = {"first", "john", "jane"};
-		String[] familyName = {"last", "doe"};
+		System.out.print (Classroom.printClass() + Classroom.classAverage());
+		
+		randomStudent();    
 
+	}
+	
+	public static String randomStudent() {
+		return firstName + Person.getFamilyName() + Student.getGPA() ;
 	}
 	
 }
