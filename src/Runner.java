1e2279f340feb9848a8ac1ffc3ae52dc48efdc8f
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-11-30 23:07:12 -0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,24 +1,61 @@
+import java.awt.Color;
+import java.util.Random;
+
 public class Runner {
 
-	private static String[] firstName = {"First", "John", "Jane", "Nathan", "Joe", "Steve", "Liam", "Noah", "Jake", "Logan", "David"};
+	private static String[] firstName = {"First", "John", "Jane", "Sophia", "Olivia", "Emma", "Nathan", "Joe", "Steve", "Liam", "Noah", "Jake", "Logan", "David"};
 	private static String[] familyName = {"Last", "Doe", "Levin", "Obama", "Asher", "Newman", "Brown", "Williams", "Paul", "Smith", "Jones"};
 	private static String[] subject = {"AP Computer Science", "Chemistry", "AP Calculus", "Macro Economics", "Physical Education", "AP United States History", "Statistics", "Algebra", "Geometry", "Global Studies"};
-	private static String[] title = {"Mr", "Mrs", "Ms", "Miss", "Dr", "Professor"};
+	private static String[] title = {"Mr", "Mrs", "Ms", "Miss", "Dr", "Professor", "Mx"};
 	
 	public static void main(String[] args) {
 	
-		Classroom Classroom = new Classroom(null, null);
 		
-		System.out.print (Classroom.printClass() + Classroom.classAverage());
+		Random r = new Random();
 		
-		randomStudent();
-	
+		int ACT = (int)(Math.random())*36+1;
+		int APClasses = (int)(Math.random()*11);
+		int IQ = (int)(Math.random()) * 200 + 1;
+		
+		Random r2 = new Random();
+		int red = r2.nextInt(256);
+		int green = r2.nextInt(256);
+		int blue = r2.nextInt(256);
+		Color eyeColor = new Color(red,green,blue);
+		
+		double GPA = (int)(Math.random() * 110 + 1);
+		
+		//variable is declared and initalized
+		Student s = new Student(firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], eyeColor , IQ , GPA, ACT, APClasses);
+		Teacher t = new Teacher(firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], eyeColor, IQ, title[r.nextInt(title.length)], 
+				subject[r.nextInt(subject.length)]);
+		
+		System.out.println(s);
+		System.out.println(t);
+		System.out.println(randomStudent());
+		
+		
 	}
 	
 	public static String randomStudent() {
-		Student s = new Student(null, null, null, 0, 0, 0, 0);
 		
-		return s.getFirstName() + s.getFamilyName() + s.getGPA();
+		Random r = new Random();
+		
+		int ACT = (int)(Math.random())*36+1;
+		int APClasses = (int)(Math.random()*11);
+		int IQ = (int)(Math.random()) * 200 + 1;
+		
+		Random r2 = new Random();
+		int red = r2.nextInt(256);
+		int green = r2.nextInt(256);
+		int blue = r2.nextInt(256);
+		Color eyeColor = new Color(red,green,blue);
+		
+		double GPA = (int)(Math.random() * 110 + 1);
+		
+		Student s = new Student(firstName[r.nextInt(firstName.length)],familyName[r.nextInt(familyName.length)], eyeColor , IQ , GPA, ACT, APClasses);
+		
+		return "Random Student Name: " + s.getFirstName() + " " + s.getFamilyName() + " " +"GPA to the nearest whole number out of 110 Weighted: " + s.getGPA();
 		}
 
 	}
